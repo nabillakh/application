62c6c1c26814abd155a3bb60df0ec1f17ffeee10
calipolis <n.lakhmissi@gmail.com>
2014-04-10 16:42:25 +0200

    Update EventController.groovy

--- grails-app/controllers/application/communication/EventController.groovy
+++ grails-app/controllers/application/communication/EventController.groovy
@@ -76,60 +76,18 @@ class EventController {
             '*' { respond eventInstance, [status: CREATED] }
         }
     }
-
-    def edit(Event eventInstance) {
-        respond eventInstance
-    }
-
+    
     @Transactional
-    def update(Event eventInstance) {
-        if (eventInstance == null) {
-            notFound()
+    def nouveauEvent() {
+
+        def eventInstance = new Event(params)
+        if (!eventInstance.save(flush: true)) {
+            render(view: "create", model: [eventInstance: eventInstance])
             return
         }
 
-        if (eventInstance.hasErrors()) {
-            respond eventInstance.errors, view:'edit'
-            return
-        }
-
-        eventInstance.save flush:true
-
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.updated.message', args: [message(code: 'Event.label', default: 'Event'), eventInstance.id])
-                redirect eventInstance
-            }
-            '*'{ respond eventInstance, [status: OK] }
-        }
-    }
-
-    @Transactional
-    def delete(Event eventInstance) {
-
-        if (eventInstance == null) {
-            notFound()
-            return
-        }
-
-        eventInstance.delete flush:true
-
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.deleted.message', args: [message(code: 'Event.label', default: 'Event'), eventInstance.id])
-                redirect action:"index", method:"GET"
-            }
-            '*'{ render status: NO_CONTENT }
-        }
-    }
-
-    protected void notFound() {
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.not.found.message', args: [message(code: 'eventInstance.label', default: 'Event'), params.id])
-                redirect action: "index", method: "GET"
-            }
-            '*'{ render status: NOT_FOUND }
-        }
-    }
+        flash.message = message(code: 'default.created.message', args: [message(code: 'event.label', default: 'Event'), eventInstance.id])
+        redirect(action: "list")
+    
+}
 }
