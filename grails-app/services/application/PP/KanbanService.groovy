71ee919c336a0cc5f079b77b93503e78bf629bf4
calipolis <n.lakhmissi@gmail.com>
2014-04-21 11:19:08 +0200

    Update KanbanService.groovy

--- grails-app/services/application/PP/KanbanService.groovy
+++ grails-app/services/application/PP/KanbanService.groovy
@@ -29,6 +29,37 @@ class KanbanService {
         }
     }
     
+    //--- pour la taglib'administration------------- 
+   // mettre a jour les listes pour filtrer au niveau d'une entreprise
+     private Kanban[] listeKanban() {
+       def maListe = []
+        maListe = Kanban.list(sort: "famille.nom", order: "desc")
+        return maListe
+    }
+    
+        //--- pour la taglib'administration------------- 
+   // mettre a jour les listes pour filtrer au niveau d'une entreprise
+     private Famille[] listeFamille() {
+       def maListe = []
+        maListe = Famille.list(sort: "nom", order: "desc")
+        return maListe
+    }
+    
+            //--- pour la taglib'administration------------- 
+   // mettre a jour les listes pour filtrer au niveau d'une entreprise
+     private Phase[] listePhase() {
+       def maListe = []
+        maListe = Phase.list(sort: "ordre", order: "desc")
+        return maListe
+    }
+            //--- pour la taglib'administration------------- 
+   // mettre a jour les listes pour filtrer au niveau d'une entreprise
+     private Ordonnancement[] listeOrdo() {
+       def maListe = []
+        maListe = Ordonnancement.list()
+        return maListe
+    }
+    
     
     
    //---------------------
@@ -62,12 +93,17 @@ class KanbanService {
     void requeteCreation(Kanban monKanban){
         try {
             def famille = monKanban.getFamille()
-            monKanban.setOrdo(famille.getOrdo())
+            // monKanban.setOrdo(famille.getOrdo())
             def ofs = OF.findByKanban(monKanban)
             if(!ofs)  {
                 for (maPhase in montrerPhasesInitiales(monKanban)) { 
                     def of = new OF(phase : maPhase, kanban : monKanban)
                     chargeInitialeOF(of)
+                    
+                    if(maPhase.ordre == 1) {
+                        monKanban.phaseActuelle = maPhase
+                    }
+                    
                     of.save()
                 }
             }
@@ -83,8 +119,8 @@ class KanbanService {
     @Transactional
     void chargeInitialeOF(OF of) {
         try{
-            float maCharge = ((of.getKanban().getFamille().getChargeStandard()) * (of.getPhase().getCleRepartition()))
-            of.setCharge(maCharge)
+            float maCharge = ((of.getKanban().getFamille().getChargePlanifiee()) * (of.getPhase().getCleRepartition()))
+            of.setChargePlanifiee(maCharge)
         }
         catch(NullPointerException n ) {
             
@@ -101,5 +137,59 @@ class KanbanService {
             return null
         }
     }
+    
+    // permet de charger la charge d'une famille en requetant les kanbans associés
+    // mettre periode temporelle
+    private Float chargeFamille(Famille famille) {
+        def listeCharge = []
+        listeCharge = Kanban.findAll("from Kanban as b where b.famille=?", [famille])
+        
+    }
+    // permet de charger la charge d'un kanban en requetant les kanbans associés
+    // mettre periode temporelle
+    private Float chargeKanbanPlanifie(Kanban kanban) {
+        def listeCharge = []
+        listeCharge = OF.findAll("from OF as b where b.kanban=?", [kanban])
+        def charge = 0
+        listeCharge.each() { monOF ->
+            charge += monOF.charge
+        }
+        return charge
+    }
+    //
+    //// permet de charger la charge realise d'un OF en requetant les imputations associées
+    // mettre periode temporelle
+    private Float chargeOFRealise(OF of) {
+        def listeCharge = []
+        listeCharge = OF.findAll("from Imputation as b where b.of=?", [of])
+        def charge = 0
+        listeCharge.each() { monImputation ->
+            // test si l'imput est déclarée comme realisee
+            if(monImputation.realise) {
+                charge += monImputation.event.durationMinutes
+            }
+        }
+        return charge
+    }    
+    //   //// permet de charger la charge deja dans l'agenda
+    private Float chargeOFAgenda(OF of) {
+        def listeCharge = []
+        listeCharge = OF.findAll("from Imputation as b where b.of=?", [of])
+        def charge = 0
+        listeCharge.each() { monImputation ->
+            charge += monImputation.event.durationMinutes
+        }
+        return charge
+    }  
+    //   rend les kanbans par famille
+    // mettre par annee aussi
+    private Kanban[] projetsFamille(Famille famille) {
+        def listeProjets = []
+        listeProjets = Kanban.findAll("from Kanban as b where b.famille=?", [famille])
+        
+        return listeProjets
+    }
+    
+    
     
 }
