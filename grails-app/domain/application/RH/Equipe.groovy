a79ade51b941bf8b21062f4a0414ecc2f93d65c9
calipolis <n.lakhmissi@gmail.com>
2014-05-21 08:46:07 +0200

    ajout hasMany dans equipe

--- grails-app/domain/application/RH/Equipe.groovy
+++ grails-app/domain/application/RH/Equipe.groovy
@@ -3,6 +3,10 @@ package application.RH
 class Equipe {
 
     String nom
+    
+    static hasMany = [effectifs : Effectif]
+    
     static constraints = {
+        effectifs nullable : true
     }
 }
