05cd20295f1cc722e8ed84c3df918368cdc54d13
nabil <n.lakhmissi@gmail.com>
2014-05-21 11:12:00 +0200

    modif modele equipe ok

--- grails-app/domain/application/RH/Equipe.groovy
+++ grails-app/domain/application/RH/Equipe.groovy
@@ -3,6 +3,11 @@ package application.RH
 class Equipe {
 
     String nom
+    
+    static hasMany = [effectifs : Effectif]
+    
+    
     static constraints = {
+        effectifs nullable : true
     }
 }
