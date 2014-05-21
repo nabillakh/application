b6f17ae381b51777ddb41f8537b45e0e1235fa59
calipolis <n.lakhmissi@gmail.com>
2014-05-21 09:22:49 +0200

    Update Equipe.groovy

--- grails-app/domain/application/RH/Equipe.groovy
+++ grails-app/domain/application/RH/Equipe.groovy
@@ -3,10 +3,6 @@ package application.RH
 class Equipe {
 
     String nom
-    
-    static hasMany = [effectifs : Effectif]
-    
     static constraints = {
-        effectifs nullable : true
     }
 }
